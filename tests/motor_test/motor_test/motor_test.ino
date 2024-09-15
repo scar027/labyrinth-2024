247e441e9ecb44511a509a8ea0ee917d33160ebd
scar027 <shantanupai@hotmail.com>
2024-09-15 14:34:32 +0530

    Add remote controlled robot code

--- tests/motor_test/motor_test/motor_test.ino
+++ tests/motor_test/motor_test/motor_test.ino
@@ -1,11 +1,3 @@
-#include <HCSR04.h>
-
-// Define pin connections for ultrasonic sensors
-#define TRIG_RIGHT 25
-#define ECHO_RIGHT 22
-#define TRIG_FRONT 26
-#define ECHO_FRONT 27
-
 // Motor control pins
 #define MOTOR_LEFT_FORWARD 23
 #define MOTOR_LEFT_BACKWARD 21
@@ -14,15 +6,7 @@
 #define ENABLE_LEFT 13
 #define ENABLE_RIGHT 14
 
-// Threshold for detecting walls (in cm)
-const int WALL_THRESHOLD = 10;
-const int RIGHT_WALL_THRESHOLD = 25;
 const int MOTOR_SPEED = 100;
-
-
-// Ultrasonic sensors setup
-UltraSonicDistanceSensor rightSensor(TRIG_RIGHT, ECHO_RIGHT);
-UltraSonicDistanceSensor frontSensor(TRIG_FRONT, ECHO_FRONT);
 
 // Motor control functions
 void moveForward() {
@@ -59,10 +43,6 @@ void turnLeft() {
     delay(700);
 }
 
-
-
-
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -77,40 +57,11 @@ void setup() {
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  
-  // right wall following robot log :
-  // state1: wall on right and nothing ahead : follow wall
-  // state2: wall on right and obstacle ahead : turn left
-  // state3: no wall on right : turn right
-
-  // Get distance readings from sensors
-  int rightDistance = rightSensor.measureDistanceCm();
-  int frontDistance = frontSensor.measureDistanceCm();
-
-  if (rightDistance <= 0) rightDistance = 0;
-  if (frontDistance <= 0) frontDistance = 0;
-
-  Serial.print("Right: "); Serial.print(rightDistance);
-  Serial.print(" | Front: "); Serial.println(frontDistance);
-
-  if (rightDistance <= RIGHT_WALL_THRESHOLD && frontDistance >= WALL_THRESHOLD){
-      moveForward();
-      Serial.println("Case 1"); // print statements for debugging
-  }
-  else if (rightDistance <= RIGHT_WALL_THRESHOLD && frontDistance <= WALL_THRESHOLD){
-      stopRobot();
-      delay(1000);
-      turnLeft();
-      Serial.println("Case 2");
-  }
-  else if (rightDistance > RIGHT_WALL_THRESHOLD){
-      stopRobot();
-      delay(1000);
-      turnRight();
-      Serial.println("Case 3");
-  }
-
+void loop()
+{
+  // moveForward();
+  // turnLeft();
+  // turnRight();
+  stopRobot();
   delay(10);
 }
